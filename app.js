f7184223c5a6861abd3092335ce10dc3cc920c9c
Rafa Calin <rafa.calin@yahoo.com>
2017-02-14 09:05:57 +0000

    initial routes

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+   res.render("landing"); 
+});
+
+app.get("/campgrounds", function(req, res){
+   var campgrounds = [
+       {name : "Seara pe Deal" , image: "http://www.photosforclass.com/download/4684194306"},
+       {name : "Pe Meses" , image: "http://www.photosforclass.com/download/2602356334"},
+       {name : "In Padure" , image: "http://www.photosforclass.com/download/2069978635"},
+    ]
+   res.render("campgrounds", {campgrounds: campgrounds}); 
+});
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server Started!")
 })
