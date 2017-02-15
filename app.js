479e6f87320f1fa3a4f0d60e16c55ebf88401b83
Rafa Calin <rafa.calin@yahoo.com>
2017-02-15 16:30:47 +0000

    am adaugat MongoDB/mongoose

--- app.js
+++ app.js
@@ -1,30 +1,47 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express     = require("express"),
+    app        = express(),
+    bodyParser = require("body-parser"),
+    mongoose   = require("mongoose")
+
+mongoose.connect("mongodb://localhost/yelp_camp"); // conecteaza baza de date
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
- var campgrounds = [
-       {name : "Seara pe Deal" , image: "https://images7.alphacoders.com/717/thumb-350-717815.jpg"},
-       {name : "Pe Meses" , image: "https://i.kinja-img.com/gawker-media/image/upload/s--ELrsBT8h--/c9pd8amxevnsn36ldwd5.jpg"},
-       {name : "In Padure" , image: "https://s-media-cache-ak0.pinimg.com/originals/7c/71/f6/7c71f6afd8bc66365da4a3ce2e98005a.jpg"},
-       {name : "Seara pe Deal" , image: "https://images7.alphacoders.com/717/thumb-350-717815.jpg"},
-       {name : "Pe Meses" , image: "https://i.kinja-img.com/gawker-media/image/upload/s--ELrsBT8h--/c9pd8amxevnsn36ldwd5.jpg"},
-       {name : "In Padure" , image: "https://s-media-cache-ak0.pinimg.com/originals/7c/71/f6/7c71f6afd8bc66365da4a3ce2e98005a.jpg"},
-       {name : "Seara pe Deal" , image: "https://images7.alphacoders.com/717/thumb-350-717815.jpg"},
-       {name : "Pe Meses" , image: "https://i.kinja-img.com/gawker-media/image/upload/s--ELrsBT8h--/c9pd8amxevnsn36ldwd5.jpg"},
-       {name : "In Padure" , image: "https://s-media-cache-ak0.pinimg.com/originals/7c/71/f6/7c71f6afd8bc66365da4a3ce2e98005a.jpg"},
-       
-    ];
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create( {name : "Pe Meses" , 
+//                     image: "https://i.kinja-img.com/gawker-media/image/upload/s--ELrsBT8h--/c9pd8amxevnsn36ldwd5.jpg"
     
+// }, function(err, campground){
+//   if(err)
+//     console.log(err)
+//     else
+//     console.log("Capm:")
+//     console.log(campground)
+// });
+
+
 app.get("/", function(req, res){
    res.render("landing"); 
 });
 
 app.get("/campgrounds", function(req, res){
   
-   res.render("campgrounds", {campgrounds: campgrounds}); 
+  Campground.find({}, function(err, allCampgrounds){
+    if (err) {
+        console.log(err);
+    } else {
+        // afiseaza toate campgrounds
+        res.render("campgrounds", {campgrounds: allCampgrounds}); 
+    }
+  });
 });
 
 app.get("/campgrounds/new", function(req, res) {
@@ -35,9 +52,15 @@ app.post("/campgrounds", function(req,res){
        var name = req.body.name;
        var image = req.body.image;
        var newCampground = {name: name, image: image};
-       
-       campgrounds.push(newCampground);
-       res.redirect("/campgrounds");
+       //Creaza un nou campground si in salveaza in DB
+        Campground.create(newCampground, function(err, newlyCreated){
+           if (err) {
+               console.log(err);
+           } else {
+               //Redirect pe /campgrounds
+                res.redirect("/campgrounds");
+           }
+        });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
