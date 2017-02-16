ae94e77d9420efbdfbe48e2261d8c6f9baf50b1d
Rafa Calin <rafa.calin@yahoo.com>
2017-02-16 18:46:54 +0000

    am adaugat More info

--- app.js
+++ app.js
@@ -11,20 +11,23 @@ app.set("view engine", "ejs");
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create( {name : "Pe Meses" , 
-//                     image: "https://i.kinja-img.com/gawker-media/image/upload/s--ELrsBT8h--/c9pd8amxevnsn36ldwd5.jpg"
+//                     image: "https://i.kinja-img.com/gawker-media/image/upload/s--ELrsBT8h--/c9pd8amxevnsn36ldwd5.jpg",
+//                     description: "La marginea orasului dsadsad dasas dasdasda"
     
 // }, function(err, campground){
 //   if(err)
 //     console.log(err)
-//     else
-//     console.log("Capm:")
-//     console.log(campground)
+//     else {
+//     console.log("Capm:");
+//     console.log(campground);
+//     }
 // });
 
 
@@ -39,7 +42,7 @@ app.get("/campgrounds", function(req, res){
         console.log(err);
     } else {
         // afiseaza toate campgrounds
-        res.render("campgrounds", {campgrounds: allCampgrounds}); 
+        res.render("index", {campgrounds: allCampgrounds}); 
     }
   });
 });
@@ -51,7 +54,9 @@ app.get("/campgrounds/new", function(req, res) {
 app.post("/campgrounds", function(req,res){
        var name = req.body.name;
        var image = req.body.image;
-       var newCampground = {name: name, image: image};
+       var desc = req.body.description;
+       
+       var newCampground = {name: name, image: image, description: desc};
        //Creaza un nou campground si in salveaza in DB
         Campground.create(newCampground, function(err, newlyCreated){
            if (err) {
@@ -61,6 +66,17 @@ app.post("/campgrounds", function(req,res){
                 res.redirect("/campgrounds");
            }
         });
+});
+
+app.get("/campgrounds/:id", function(req, res) {
+    //gaseste Campgrounds dupa id
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err)
+            console.log(err);
+        else
+            //trimite campground care corespunde la id
+            res.render("show", {campground: foundCampground});
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
